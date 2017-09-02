f67d0c98ecc9d13815bec8cfacdf332abbd2db20
Robert Kahren II <rkahren2@gmail.com>
2017-09-02 10:54:38 -0500

    added detail action method

--- src/ComicBoocGallery/Controllers/ComicBooksController.cs
+++ src/ComicBoocGallery/Controllers/ComicBooksController.cs
@@ -8,5 +8,9 @@ namespace ComicBoocGallery.Controllers
 {
     public class ComicBooksController: Controller
     {
+        public string Detail()
+        {
+            return "Hello from CommicBook Controller";
+        }
     }
 }
