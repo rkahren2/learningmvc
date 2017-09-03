7c2f21c6149c82bfc8f2cf5da7ada55ca28f0a75
Robert Kahren II <rkahren2@gmail.com>
2017-09-03 13:43:21 -0500

    updated comic book detial view for string type

--- src/ComicBoocGallery/Controllers/ComicBooksController.cs
+++ src/ComicBoocGallery/Controllers/ComicBooksController.cs
@@ -1,29 +1,35 @@
-﻿using System;
+﻿using ComicBookGallery.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
-namespace ComicBoocGallery.Controllers
+namespace ComicBookGallery.Controllers
 {
     public class ComicBooksController: Controller
     {
         public ActionResult Detail()
         {
-            ViewBag.SeriesTitle = "The amazing Spider-man";
-            ViewBag.IssueNumber = 700;
-            ViewBag.Description = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>";
-            ViewBag.Artists = new string[]
+            var comicBook = new ComicBook()
             {
-                "Script: Dan Slott",
-                "Pencils: Humberto Ramos",
-                "Inks: Victor Olazaba",
-                "Colors: Edgar Delgado",
-                "Letters: Chris Eliopoulos"
+                SeriesTitle = "The amazing Spider-man",
+                IssueNumber = 700,
+                DescriptionHtml = "<p>Final issue! Witness the final hours of Doctor Octopus' life and his one, last, great act of revenge! Even if Spider-Man survives... <strong>will Peter Parker?</strong></p>",
+                Artists = new Artist[]
+                {
+                    new Artist() {Name = "Dan Slott", Role = "Script"},
+                    new Artist() {Name = "Humberto Ramos", Role = "Pencils"},
+                    new Artist() {Name = "Victor Olazaba", Role = "Inks"},
+                    new Artist() {Name = "Edgar Delgado", Role = "Colors"},
+                    new Artist() {Name = "Chris Eliopoulos", Role = "Letters"},
+                }
             };
 
+           
 
-            return View();
+
+            return View(comicBook);
         }
     }
 }
